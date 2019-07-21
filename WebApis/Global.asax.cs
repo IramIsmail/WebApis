bd554c7cd9b7e8516c72179cd45d2d91c956f070
Iram Ismail <iram.8859@gmail.com>
2019-07-21 12:13:32 +0530

    Setup for Heroku

--- WebApis/Global.asax.cs
+++ WebApis/Global.asax.cs
@@ -18,6 +18,8 @@ namespace WebApis
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
         }
     }
 }
